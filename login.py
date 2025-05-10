af0733a16ab859ecf09905a90165c71c387967a1
111inhistory <59101115+111inhistory@users.noreply.github.com>
2025-05-10 13:11:25 +0800

    init

---
+++ login.py
@@ -0,0 +1,387 @@
+from typing import Dict, Optional
+from requests.sessions import Session
+from requests import Request, Response
+from hashlib import sha256
+import time
+import logging
+import argparse  # 添加argparse模块用于命令行参数解析
+import sys  # 添加sys模块用于访问命令行参数
+
+logging.basicConfig(
+    level=logging.ERROR,
+    format="[%(asctime)s][%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+logger = logging.getLogger(__name__)
+
+host = "http://192.168.0.1"
+goform_get_path = "/goform/goform_get_cmd_process"
+goform_set_path = "/goform/goform_set_cmd_process"
+API_GET_URL = host + goform_get_path
+API_SET_URL = host + goform_set_path
+password = "admin"
+HEADERS = {
+    "X-Requested-With": "XMLHttpRequest",
+    "Accept": "application/json, text/javascript, */*; q=0.01",
+    "Referer": "http://192.168.0.1/index.html",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0",
+}
+
+session = Session()
+
+
+class ServerError(Exception):
+    """Exception raised for server errors."""
+
+    pass
+
+
+# region
+"""
+cmd_list = [
+    "wifi_onoff_state",
+    "wifi_lbd_enable",
+    "guest_switch",
+    "wifi_chip1_ssid2_max_access_num",
+    "m_SSID2",
+    "wifi_chip2_ssid2_max_access_num",
+    "wifi_chip1_ssid1_wifi_coverage",
+    "apn_interface_version",
+    "m_ssid_enable",
+    "imei",
+    "network_type",
+    "rssi",
+    "rscp",
+    "lte_rsrp",
+    "imsi",
+    "sim_imsi",
+    "cr_version",
+    "wa_version",
+    "hardware_version",
+    "web_version",
+    "wa_inner_version",
+    "wifi_chip1_ssid1_max_access_num",
+    "iccid",
+    "wifi_chip1_ssid1_ssid",
+    "wifi_chip1_ssid1_auth_mode",
+    "wifi_chip1_ssid1_password_encode",
+    "wifi_chip2_ssid1_ssid",
+    "wifi_chip2_ssid1_auth_mode",
+    "m_HideSSID",
+    "wifi_chip2_ssid1_password_encode",
+    "wifi_chip2_ssid1_max_access_num",
+    "lan_ipaddr",
+    "lan_ipaddr",
+    "mac_address",
+    "msisdn",
+    "LocalDomain",
+    "wan_ipaddr",
+    "static_wan_ipaddr",
+    "ipv6_wan_ipaddr",
+    "ipv6_pdp_type",
+    "ipv6_pdp_type_ui",
+    "pdp_type",
+    "pdp_type_ui",
+    "opms_wan_mode",
+    "queryWiFiModuleSwitch",
+    "opms_wan_auto_mode",
+    "OOM_TEMP_PRO",
+    "privacy_read_flag",
+    "LD",
+    "ppp_status",
+    "Z5g_snr",
+    "Z5g_rsrp",
+    "wan_lte_ca",
+    "lte_ca_pcell_band",
+    "lte_ca_pcell_bandwidth",
+    "lte_ca_scell_band",
+    "lte_ca_scell_bandwidth",
+    "lte_ca_pcell_arfcn",
+    "lte_ca_scell_arfcn",
+    "lte_multi_ca_scell_info",
+    "queryWiFiModuleSwitch",
+    "wan_active_band",
+    "wifi_onoff_state",
+    "guest_switch",
+    "web_wifi_password_init_flag", 
+    "wifi_chip1_ssid2_max_access_num",
+    "wifi_chip2_ssid2_max_access_num",
+    "wifi_chip1_ssid1_wifi_coverage",
+    "wifi_chip1_ssid1_max_access_num",
+    "wifi_chip1_ssid1_ssid",
+    "wifi_chip1_ssid1_auth_mode",
+    "wifi_chip1_ssid1_password_encode",
+    "wifi_chip2_ssid1_ssid",
+    "wifi_chip2_ssid1_auth_mode",
+    "wifi_chip2_ssid1_password_encode",
+    "wifi_chip2_ssid1_max_access_num",
+    "wifi_chip1_ssid2_ssid",
+    "wifi_chip2_ssid2_ssid",
+    "wifi_chip1_ssid1_switch_onoff",
+    "wifi_chip2_ssid1_switch_onoff",
+    "wifi_chip1_ssid2_switch_onoff",
+    "wifi_chip2_ssid2_switch_onoff",
+    "Z5g_SINR",
+    "station_ip_addr",
+    "opms_wan_mode",
+    "opms_wan_auto_mode",
+    "loginfo",
+    "ppp_status",
+    "ethernet_port_specified",
+    "modem_main_state", 
+    "puknumber", 
+    "pinnumber", 
+    "opms_wan_mode", 
+    "psw_fail_num_str", 
+    "login_lock_time", 
+    "SleepStatusForSingleChipCpe"
+]
+
+single_cmd_list = [
+    "queryAccessPointInfo",
+]
+"""
+# endregion
+
+
+def set_logger(log_level, log_file=None):
+    """设置日志级别和日志文件（如果提供）"""
+    numeric_level = getattr(logging, log_level.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError(f"Invalid log level: {log_level}")
+
+    log_handlers = []
+
+    # 创建控制台处理器
+    console_handler = logging.StreamHandler()
+    log_handlers.append(console_handler)
+
+    # 如果提供了日志文件，创建文件处理器
+    if log_file:
+        file_handler = logging.FileHandler(log_file, encoding="utf-8")
+        log_handlers.append(file_handler)
+
+    # 配置根日志记录器
+    logging.basicConfig(
+        level=numeric_level,
+        format="[%(asctime)s][%(levelname)s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        handlers=log_handlers,
+        force=True,
+    )
+
+    logger.setLevel(numeric_level)
+    logger.info(f"日志级别设置为: {log_level}")
+    if log_file:
+        logger.info(f"日志将写入文件: {log_file}")
+
+
+def prepare_params(args: list) -> Dict[str, str]:
+    if len(args) == 0:
+        return {}
+    params = {
+        "isTest": False,
+    }
+    if len(args) == 1:
+        params["cmd"] = args[0]
+    else:
+        params["cmd"] = ",".join(args)
+        params["multi_data"] = 1
+    params["_"] = str(int(time.time() * 1000))
+    logger.debug(f"get request params: {params}")
+    return params
+
+
+def send(req: Request, *, try_time: int = 3) -> Optional[Response]:
+    try:
+        status = verify_session()
+    except ServerError as e:
+        return None
+    if not status and try_time > 0:
+        logger.info("Session expired, re-login.")
+        login()
+
+    logger.debug(f"send request: {req.url}")
+    logger.debug(f"send request method: {req.method}")
+    logger.debug(f"send request params: {req.params}")
+    logger.debug(f"send request data: {req.data}")
+    logger.debug(f"send request headers: {req.headers}")
+    logger.debug(f"send request cookies: {session.cookies.get_dict()}")
+    try:
+        res = session.send(req.prepare(), timeout=5)
+    except Exception as e:
+        logger.error(f"Request failed: {e}")
+        return None
+    if res.status_code == 401 and try_time > 0:
+        relogin = False
+        while not relogin:
+            if try_time <= 0:
+                logger.error("Failed to re-login.")
+                return None
+            logger.info(f"Session expired, re-login. {try_time} times left.")
+            try_time -= 1
+            relogin = login()
+        if "_" in req.params:
+            req.params["_"] = str(int(time.time() * 1000))
+        return send(req, try_time=try_time)
+    elif res.status_code != 200:
+        logger.error(f"Request failed: {res.status_code}")
+        return None
+    return res
+
+
+def verify_session():
+    cmd = [
+        "opms_wan_mode",
+        "opms_wan_auto_mode",
+        "loginfo",
+        "ppp_status",
+        "ethernet_port_specified",
+    ]
+    res = session.get(url=API_GET_URL, params=prepare_params(cmd), headers=HEADERS)
+    logger.debug(f"verify session {res.url}:")
+    logger.debug(f"status code: {res.status_code}")
+    logger.debug(f"response: {res.text}")
+    logger.debug(f"cookie: {session.cookies.get_dict()}")
+    if res.status_code != 200:
+        raise ServerError("The server is not responding.")
+    elif res.json().get("loginfo") == "ok":
+        return True
+    else:
+        return False
+
+
+def login():
+    # get param LD
+    cmd = ["LD"]
+    req = Request(
+        method="GET",
+        url=API_GET_URL,
+        params=prepare_params(cmd),
+        headers=HEADERS,
+    )
+    logger.debug(f"getting LD param")
+    res: Optional[Response] = send(req, try_time=0)
+    if res is None:
+        logger.error("Failed to get response from the server.")
+        return False
+    LD: str = res.json().get("LD")
+    logger.debug(f"LD param: {LD}")
+    if not LD:
+        logger.error("Failed to get LD param.")
+        return False
+
+    # Construct login request
+    form = {
+        "isTest": False,
+        "goformId": "LOGIN",
+    }
+    form["password"] = (
+        sha256(
+            (sha256(password.encode("utf-8")).hexdigest().upper() + LD).encode("utf-8")
+        )
+        .hexdigest()
+        .upper()
+    )
+    # res = session.post(url=API_SET_URL, data=form, headers=HEADERS)
+    req = Request(
+        method="POST",
+        url=API_SET_URL,
+        data=form,
+        headers=HEADERS,
+    )
+    res = send(req, try_time=0)
+    if res is None:
+        logger.error("Failed to get response from the server.")
+        return False
+
+    if res.json().get("result") == 0:
+        logger.info("Login successful.")
+        return True
+    else:
+        logger.error("Login failed.")
+        return False
+
+
+def get_ipv6_addr():
+    cmd = ["ipv6_wan_ipaddr"]
+    req = Request(
+        method="GET",
+        url=API_GET_URL,
+        params=prepare_params(cmd),
+        headers=HEADERS,
+    )
+    res = send(req)
+    if res is None:
+        logger.error("Failed to get response from the server.")
+        return None
+    else:
+        ipv6_addr = res.json().get("ipv6_wan_ipaddr")
+        logger.info(f"Current WAN IPv6 addr: {ipv6_addr}")
+        return ipv6_addr
+
+
+def modify_const(**kwargs):
+    """修改常量值"""
+    global API_GET_URL, API_SET_URL, host, password
+    if "host" in kwargs and kwargs["host"]:
+        host = kwargs["host"]
+    if "port" in kwargs and kwargs["port"]:
+        host = f"{host}:{kwargs['port']}"
+    API_GET_URL = host + goform_get_path
+    API_SET_URL = host + goform_set_path
+    if "password" in kwargs and kwargs["password"]:
+        password = kwargs["password"]
+
+
+operations = {
+    "login": login,
+    "ipv6": get_ipv6_addr,
+}
+
+if __name__ == "__main__":
+    # 解析命令行参数
+    """解析命令行参数"""
+    parser = argparse.ArgumentParser(description="中兴F50操作脚本")
+    parser.add_argument(
+        "operation", type=str, help="要执行的操作（login, get_ipv6_addr 等）"
+    )
+    parser.add_argument("password", type=str, help="登录密码")
+    parser.add_argument(
+        "-H", "--host", type=str, default=host, help="路由器的主机名或IP地址"
+    )
+    parser.add_argument("-p", "--port", type=int, default=80, help="路由器的端口号")
+    parser.add_argument(
+        "--LOG_LEVEL",
+        type=str,
+        default="ERROR",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        help="日志级别",
+    )
+    parser.add_argument("--log_file", type=str, help="日志文件路径")
+
+    args = parser.parse_args()
+
+    # 设置日志级别和文件
+    set_logger(args.LOG_LEVEL, args.log_file)
+
+    if args.host:
+        host = args.host
+    if args.port:
+        host = f"{host}:{args.port}"
+    API_GET_URL = host + goform_get_path
+    API_SET_URL = host + goform_set_path
+
+    password = args.password
+
+    # 根据操作选择执行的函数
+
+    if args.operation in operations:
+        result = operations[args.operation]()
+        if result is not None:
+            print(result)
+    else:
+        logger.error(f"未知操作: {args.operation}")
+        logger.info(f"可用操作: {", ".join(operations.keys())}")
+        sys.exit(1)
---
+++ main.py
@@ -0,0 +1,212 @@
+import time
+import login
+import cloudflare
+import logging
+import argparse
+
+from typing import Any, List, Tuple, Optional
+
+# 获取logger
+logger = logging.getLogger(__name__)
+
+RRTYPE = "AAAA"
+refresh_time = 60
+zone_id = {}
+dns_id = {}
+
+
+def set_logger(log_level, log_file=None):
+    """设置日志级别和日志文件（如果提供）"""
+    numeric_level = getattr(logging, log_level.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError(f"Invalid log level: {log_level}")
+
+    log_handlers = []
+
+    # 创建控制台处理器
+    console_handler = logging.StreamHandler()
+    log_handlers.append(console_handler)
+
+    # 如果提供了日志文件，创建文件处理器
+    if log_file:
+        file_handler = logging.FileHandler(log_file, encoding="utf-8")
+        log_handlers.append(file_handler)
+
+    # 配置根日志记录器
+    logging.basicConfig(
+        level=numeric_level,
+        format="[%(asctime)s][%(levelname)s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        handlers=log_handlers,
+        force=True,
+    )
+
+    logger.setLevel(numeric_level)
+    logger.info(f"日志级别设置为: {log_level}")
+    if log_file:
+        logger.info(f"日志将写入文件: {log_file}")
+
+
+def create_client(api_token: str) -> bool:
+    try:
+        client = cloudflare.Cloudflare(api_token=api_token)
+    except cloudflare.CloudflareError as e:
+        logger.error("Verify token failed. Please check your token")
+        logger.debug(f"Error: {e}")
+        logger.debug(f"api_token: {api_token}")
+        exit(1)
+    return client
+
+
+def get_zone_id(client: cloudflare.Cloudflare, zone: str) -> str:
+    global zone_id
+    if zone in zone_id:
+        return zone_id[zone]
+    result = client.zones.list(name=zone)
+    if result.result:
+        zone_id = result.result[0].id
+        logger.debug(f"Zone id of {zone} found {zone_id}")
+        return zone_id
+    logger.error(f"Get zone id of {zone} failed. Please check your zone name")
+    return ""
+
+
+def get_dns_id(
+    client: cloudflare.Cloudflare, zone_id: str, dns_name: str
+) -> str:
+    global dns_id
+    if dns_name in dns_id:
+        return dns_id[dns_name]
+    dns_records = client.dns.records.list(zone_id=zone_id, name=dns_name, type=RRTYPE)
+    if not dns_records:
+        return None
+    result = dns_records.result
+    for i in result:
+        if i.name == dns_name and i.type == RRTYPE:
+            logger.debug(f"DNS id of {dns_name} found {i.id}")
+            return i.id
+    return None
+
+
+def get_dns_record(client: cloudflare.Cloudflare, zone_id: str, dns_id: str) -> Any:
+    result = client.dns.records.get(zone_id=zone_id, dns_record_id=dns_id)
+    
+
+    if not result:
+        return None
+    if result.id == dns_id and result.type == RRTYPE:
+        logger.debug(f"DNS record {dns_id} found")
+        logger.debug(f"DNS record {dns_id} content: {result.content}")
+        logger.debug(f"DNS record {dns_id} name: {result.name}")
+        logger.debug(f"DNS record {dns_id} type: {result.type}")
+        logger.debug(f"DNS record {dns_id} ttl: {result.ttl}")
+        logger.debug(f"DNS record {dns_id} proxied: {result.proxied}")
+        logger.debug(f"DNS record {dns_id} comment: {result.comment}")
+        return result
+    return None
+
+
+def create_dns_record(client: cloudflare.Cloudflare, zone_id, dns_name, ip_address):
+    client.dns.records.create(
+        zone_id=zone_id,
+        name=dns_name,
+        type=RRTYPE,
+        content=ip_address,
+        ttl=60,
+        proxied=False,
+        comment="Created for IPv6 DDNS",
+    )
+
+
+def do_dns_update(
+    client: cloudflare.Cloudflare,
+    zone_name,
+    dns_name,
+    ip_address,
+):
+    zone_id = get_zone_id(client, zone_name)
+    dns_id = get_dns_id(client, zone_id, dns_name)
+    if not dns_id:
+        logger.info(f"DNS record {dns_name} not found, creating a new one")
+        create_dns_record(client, zone_id, dns_name, ip_address)
+        dns_id = get_dns_id(client, zone_id, dns_name)
+        if not dns_id:
+            logger.error(f"Failed to create DNS record {dns_name}")
+            return False
+    record = get_dns_record(client, zone_id, dns_id)
+    if record.content == ip_address:
+        logger.info(f"DNS record {dns_name} is up to date")
+        return True
+    else:
+        logger.info(f"DNS record {dns_name} is not up to date, updating it")
+    client.dns.records.edit(
+        zone_id=zone_id,
+        dns_record_id=dns_id,
+        content=ip_address,
+    )
+    logger.info(f"DNS record {dns_name} updated to {ip_address} from {record.content}")
+    return True
+
+
+# This function gets IPv6 address using dig method.
+def get_ipv6_address() -> str:
+    return login.get_ipv6_addr()
+
+
+def main(api_key, zone_name, subdomain, refresh_time):
+    logger.info("Starting DDNS update main loop")
+    # 创建Cloudflare客户端
+    client = create_client(api_key)
+
+    ipv6_addr = get_ipv6_address()
+    dns_name = f"{subdomain}.{zone_name}"
+
+    logger.info(f"DNS name: {dns_name}")
+    logger.info(f"Current IPv6 address: {ipv6_addr}")
+
+    do_dns_update(client, zone_name, dns_name, ipv6_addr)
+    while True:
+        time.sleep(refresh_time)
+        ipv6_addr = get_ipv6_address()
+        logger.info(f"Current IPv6 address: {ipv6_addr}")
+        do_dns_update(client, zone_name, dns_name, ipv6_addr)
+
+
+if __name__ == "__main__":
+    # 解析命令行参数
+    parser = argparse.ArgumentParser(description="Cloudflare DDNS IPv6更新工具")
+    parser.add_argument(
+        "router_password", type=str, help="路由器登录密码"
+    )
+    required = parser.add_argument_group("必选参数")
+    required.add_argument("-a", "--api_key", type=str, required=True, help="Cloudflare API密钥")
+    required.add_argument("-z", "--zone_name", type=str,required=True, help="域名区域名称，如example.com")
+    required.add_argument("-n", "--subdomain", required=True, type=str, help="子域名，如www")
+    optional = parser.add_argument_group("可选参数")
+    optional.add_argument(
+        "-H", "--router_host", type=str, default="http://192.168.0.1", help="路由器IP地址"
+    )
+    optional.add_argument("-p", "--router_port", type=int, default=80, help="路由器端口号")
+    optional.add_argument("-t", "--refresh_time", type=int, default=60, help="刷新时间（秒）")
+    optional.add_argument(
+        "--LOG_LEVEL",
+        type=str,
+        default="INFO",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        help="日志级别",
+    )
+    optional.add_argument("--log_file", type=str, help="日志文件路径")
+
+    args = parser.parse_args()
+
+    # 设置日志级别和文件
+    set_logger(args.LOG_LEVEL, args.log_file)
+
+    # 设置路由器连接参数
+    login.modify_const(
+        host=args.router_host,
+        port=args.router_port,
+        password=args.router_password
+    )
+
+    main(args.api_key, args.zone_name, args.subdomain, args.refresh_time)
